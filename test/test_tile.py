89f586de055a2f96167bd4f45f7dda91e87eeb78
DiegoXuHuang <diego@diegodeMacBook-Air.local>
2023-08-28 22:30:38 -0300

    add test correct for tile

--- test/test_tile.py
+++ test/test_tile.py
@@ -4,8 +4,8 @@ from game.tile import Tile
 class TestTile(unittest.TestCase):
 
     def test_get_value(self):
-        self.assertEqual(self.tile_A.get_value(), 1)
-        self.assertEqual(self.tile.get_value(), 10)
+        tile = Tile("A", 1)
+        self.assertEqual(tile.get_value(), 1)
    
 
     def test_get_letter(self):
