c2e67862394780bcbd574b27832aa6e16ccc07f2
DiegoXuHuang <diego@diegodeMacBook-Air.local>
2023-09-06 09:43:10 -0300

    test

--- game/calculate.py
+++ game/calculate.py
@@ -1,8 +1,8 @@
-class Calculate_word_value:
-    # def __call__(word):
-    #     value = 0
-    #     for cell in word:
-    #         value += cell.letter.value
-    #     return value
-    pass
+# class Calculate_word_value:
+#     # def __call__(word):
+#     #     value = 0
+#     #     for cell in word:
+#     #         value += cell.letter.value
+#     #     return value
+#     pass
 
