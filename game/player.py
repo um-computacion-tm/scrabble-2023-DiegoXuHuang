c0ddc236f4bee61437d2cc17af7d50667ba82cc4
DiegoXuHuang <diego@diegodeMacBook-Air.local>
2023-09-29 22:36:41 -0300

    add validate word and letter

--- game/player.py
+++ game/player.py
@@ -1,11 +1,24 @@
 class Player:
-    def __init__(self, puntuacion = 0):
+    def __init__(self, score = 0):
         self.tiles = []
-        self.puntuacion = puntuacion
+        self.score = score
+    #nuevo 
+    def validate_letter(self, letter):
+        if letter in self.tiles:
+            return True
+        return False
 
-#     def __init__(self)
+    # def __init__(self, bag_tiles):
+    #     self.tiles = bag_tiles.take(7)
+    #     self.bag_tiles = bag_tiles
 
+    # def rellenar(self, bag_tiles):
+    #     self.tiles += bag_tiles.take(
+    #         7 - len(self.tiles)
+    #     )
    
+    # def has_letter(self):
+    #     pass
 
 
 # terminar turnos de los jugadores
