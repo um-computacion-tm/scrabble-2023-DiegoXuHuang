010d8000f470375ea4e73bab62ff8028356e9802
DiegoXuHuang <diego@diegodeMacBook-Air.local>
2023-10-25 14:40:36 -0300

    add method if board full

--- game/scrabble.py
+++ game/scrabble.py
@@ -1,70 +1,73 @@
-# from game.board import Board
-# from game.player import Player
-# from game.models import BagTiles
-# from game.dictionary import * 
-# from game.cell import *
+from game.board import Board
+from game.player import Player
+from game.models import BagTiles
+from game.dictionary import * 
+from game.cell import *
 
-# class DictionaryConnectionError(Exception):
-#     pass
-# class InvalidWordException(Exception):
-#     pass
-# class InvalidPlaceWordException(Exception):
-#     pass
-
-
-
-# class ScrabbleGame:
-#     def __init__(self, players_count):
-#         self.board = Board()
-#         self.bag_tiles = BagTiles()
-#         self.players = []
-#         for _ in range(players_count):
-#             self.players.append(Player())
-
-#         self.current_player = None
-#         self.turn = 0
-#         self.board.add_premium_cells()
+class DictionaryConnectionError(Exception):
+    pass
+class InvalidWordException(Exception):
+    pass
+class InvalidPlaceWordException(Exception):
+    pass
 
 
-#     # falta test de playing
-#     def playing(self):
-#         return True
+
+class ScrabbleGame:
+    def __init__(self, players_count):
+        self.board = Board()
+        self.bag_tiles = BagTiles()
+        self.players = []
+        for _ in range(players_count):
+            self.players.append(Player())
+
+        self.current_player = None
+        self.turn = 0
+        self.board.add_premium_cells()
+     
+
+    # falta test de playing
+    def playing(self):
+        return True
     
-#     # falta definir  
-#     def play(self, word, location, orientation):
-#         self.validate_word(word, location, orientation)
-#         words = self.board.put_words(word, location, orientation)
-#         total = self.calculate_words_value(words)
-#         self.players[self.current_player].score += total
-#         self.next_turn()
+    # falta definir  
+    # def play(self, word, location, orientation):
+    #     self.validate_word(word, location, orientation)
+    #     words = self.board.put_words(word, location, orientation)
+    #     total = self.calculate_words_value(words)
+    #     self.players[self.current_player].score += total
+    #     self.next_turn()
+    #     self.dictionary = Dictionary('dictionaries/dictionary.txt')
 
+
+    def next_turn(self):
+        if self.current_player is None:
+            self.current_player = self.players[0]
+        else:
+            index = (self.players.index(self.current_player) + 1) % len(self.players)
+            self.current_player = self.players[index]
+# 
+    #falta definir
+
+    # def validate_word(self, word, location, orientation):
+    #     if not dict_validate_word(word):
+    #         raise InvalidWordException("Su palabra no existe en el diccionario")s
+    #     if not self.board.validate_word_inside_board(word, location, orientation):
+    #         raise InvalidPlaceWordException("Su palabra excede el tablero")
+    #     if not self.board.validate_word_place_board(word, location, orientation):
+    #         raise InvalidPlaceWordException("Su palabra esta mal puesta en el tablero")
 
     
-#     def next_turn(self):
-#         if self.current_player is None:
-#             self.current_player = self.players[0]
-#         else:
-#             index = (self.players.index(self.current_player) + 1) % len(self.players)
-#             self.current_player = self.players[index]
-# # 
-#     #falta definir
 
-#     def validate_word(self, word, location, orientation):
-#         if not dict_validate_word(word):
-#             raise InvalidWordException("Su palabra no existe en el diccionario")
-#         if not self.board.validate_word_inside_board(word, location, orientation):
-#             raise InvalidPlaceWordException("Su palabra excede el tablero")
-#         if not self.board.validate_word_place_board(word, location, orientation):
-#             raise InvalidPlaceWordException("Su palabra esta mal puesta en el tablero")
+    def is_board_full(self):
+        cell_has_letter = [cell.letter is not None for row in self.board.grid for cell in row]
+        return all(cell_has_letter)
+    
 
- 
-
-
-
-#     def end_game(self):
-#         if len(self.bag_tiles) == 0:
-#             return True
-#         return False
+    def end_game(self):
+        if len(self.bag_tiles) == 0:
+            return True
+        return False
     
 
 
@@ -77,25 +80,29 @@
 
 
 
-#     # def validate_word(self, word, location, orientation):
-#     #     
-#         #   '''
-#         #   1- Validar que usuario tiene esas letras
-#         #   2- Validar que la palabra entra en el tablero
-#         #   '''
-#     #     self.board.validate_word_inside_board(word, location, orientation)
+    # def validate_word(self, word, location, orientation):
+    #     
+        #   '''
+        #   1- Validar que usuario tiene esas letras
+        #   2- Validar que la palabra entra en el tablero
+        #   '''
+    #     self.board.validate_word_inside_board(word, location, orientation)
     
 
-#     #---------------------------
-#     # def get_words():
-#     #     '''
-#     #     Obtener las posibles palabras que se pueden formar, dada una palabra, ubicacion y orientacion 
-#     #     Preguntar al usuario, por cada una de esas palabras, las que considera reales
-#     #     '''
+    #---------------------------
+    # def get_words():
+    #     '''
+    #     Obtener las posibles palabras que se pueden formar, dada una palabra, ubicacion y orientacion 
+    #     Preguntar al usuario, por cada una de esas palabras, las que considera reales
+    #     '''
     
-#     # def put_words():
-#     #     '''
-#     #     Modifica el estado del tablero con las palabras consideradas como correctas
-#     #     '''
+    # def put_words():
+    #     '''
+    #     Modifica el estado del tablero con las palabras consideradas como correctas
+    #     '''
+
+
+
+
 
 
--- test/test_scrabble_game.py
+++ test/test_scrabble_game.py
@@ -1,52 +1,67 @@
-# import unittest
-# from game.scrabble import ScrabbleGame
-# from game.tile import Tile
+import unittest
+from game.scrabble import ScrabbleGame
+from game.tile import Tile
 
 
-# class TestScrabbleGame(unittest.TestCase):
-#     def test_init(self):
-#         scrabble_game = ScrabbleGame(players_count=3)
-#         self.assertIsNotNone(scrabble_game.board)
-#         self.assertEqual(
-#             len(scrabble_game.players),
-#             3,
-#         )
-#         self.assertIsNotNone(scrabble_game.bag_tiles)
 
-#     def test_playing(self):
-#         game = ScrabbleGame(players_count=2)
-#         self.assertTrue(game.playing())
+class TestScrabbleGame(unittest.TestCase):
+    def test_init(self):
+        scrabble_game = ScrabbleGame(players_count=3)
+        self.assertIsNotNone(scrabble_game.board)
+        self.assertEqual(
+            len(scrabble_game.players),
+            3,
+        )
+        self.assertIsNotNone(scrabble_game.bag_tiles)
+
+    def test_playing(self):
+        game = ScrabbleGame(players_count=2)
+        self.assertTrue(game.playing())
     
  
     
-#     def test_next_turn_when_game_is_starting(self):
-#         scrabble_game = ScrabbleGame(players_count=3)
+    def test_next_turn_when_game_is_starting(self):
+        scrabble_game = ScrabbleGame(players_count=3)
 
-#         scrabble_game.next_turn()
+        scrabble_game.next_turn()
 
-#         assert scrabble_game.current_player == scrabble_game.players[0]
+        assert scrabble_game.current_player == scrabble_game.players[0]
 
 
-#     def test_next_turn_when_player_is_not_the_first(self):
-#         scrabble_game = ScrabbleGame(players_count=3)
-#         scrabble_game.current_player = scrabble_game.players[0]
+    def test_next_turn_when_player_is_not_the_first(self):
+        scrabble_game = ScrabbleGame(players_count=3)
+        scrabble_game.current_player = scrabble_game.players[0]
 
-#         scrabble_game.next_turn()
+        scrabble_game.next_turn()
 
-#         assert scrabble_game.current_player == scrabble_game.players[1]
+        assert scrabble_game.current_player == scrabble_game.players[1]
 
-#     def test_next_turn_when_player_is_last(self):
-#         scrabble_game = ScrabbleGame(players_count=3)
-#         scrabble_game.current_player = scrabble_game.players[2]
+    def test_next_turn_when_player_is_last(self):
+        scrabble_game = ScrabbleGame(players_count=3)
+        scrabble_game.current_player = scrabble_game.players[2]
 
-#         scrabble_game.next_turn()
+        scrabble_game.next_turn()
 
-#         assert scrabble_game.current_player == scrabble_game.players[0]
+        assert scrabble_game.current_player == scrabble_game.players[0]
 
-#     def test_end_game(self):
-#         scrabble_game = ScrabbleGame(players_count=4)
-#         scrabble_game.bag_tiles = []  
-#         self.assertTrue(scrabble_game.end_game())
+
+
+    def test_full_board(self):
+        scrabble_game = ScrabbleGame(players_count=2)
+
+        for i in range(7):
+            for j in range(15):
+                scrabble_game.board.grid[i][j].letter = Tile("T", 1)
+
+        is_board_full = scrabble_game.is_board_full()
+        self.assertFalse(is_board_full)
+
+
+
+    def test_end_game(self):
+        scrabble_game = ScrabbleGame(players_count=4)
+        scrabble_game.bag_tiles = []  
+        self.assertTrue(scrabble_game.end_game())
 
 
 
@@ -58,5 +73,5 @@
         
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
