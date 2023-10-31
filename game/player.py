c4ba7621779c09521cd732f26cfecf6366552181
DiegoXuHuang <diego@diegodeMacBook-Air.local>
2023-10-30 21:59:14 -0300

    update

--- game/player.py
+++ game/player.py
@@ -1,26 +1,40 @@
 
 from game.tile import Tile
 from game.models import BagTiles
+from game.models import BagTiles
+from game.util import Util
 
-#falta ponerlo
+
+
 class NoSuficienteFichasException(Exception):
     pass
 
 class Player:
-    def __init__(self, score = 0,):
+    def __init__(self):
         self.tiles = []
-        self.score = score  
+        self.score = 0  
         self.name = ""
-        self.current_player = None
+        #self.current_player = self
+        self.bag_tiles = BagTiles()
+        self.util = Util()
+
+
+     
+    def refill(self):
+        max_tiles_to_take = 7 - len(self.tiles)
+        if max_tiles_to_take > 0:
+            tiles_to_take = self.bag_tiles.take(max_tiles_to_take)
+            self.tiles.extend(tiles_to_take)
+
+
+    def add_tile(self, tile:Tile):
+        self.tiles.append(tile)
         
     def set_name(self, name):
         self.name = name
     
     def get_name(self):
         return self.name
-
-    # def get_tiles(self, bag: BagTiles, amount):
-    #     self.tiles += bag.take(amount)
 
     def show_tiles(self):
         return self.tiles
@@ -35,20 +49,15 @@ class Player:
 
     def get_score(self):
         return self.score
-    
-    def refill(self, bag: BagTiles):
-        # Calcula la cantidad de fichas que se necesitan para llegar a un total de 7.
-        # rellenar las fichas de un jugador para que tengan 7 fichas en su mano
-        tiles_needed = 7 - len(self.tiles)
-        new_tiles = bag.take(tiles_needed)
-        self.tiles.extend(new_tiles)
- 
+
 
     def has_letters(self, letter_set):
         letter_inventory = {tile.letter: 0 for tile in self.tiles}
 
-        if isinstance(letter_set, str):
+        
+        if self.util.is_word_set(str):
             letter_set = letter_set.upper()
+       
         elif all(isinstance(item, str) for item in letter_set):
             letter_set = [letter.upper() for letter in letter_set]
         else:
@@ -64,7 +73,8 @@ class Player:
                 return False
 
         return True
-
+    
+   
         
     def exchange_tile(self, bag: BagTiles, tile_exchange):
         if tile_exchange in self.tiles:
@@ -73,12 +83,14 @@ class Player:
             bag.shuffle_bag()
             self.tiles.append(bag.take(1)[0])
     
+    
     def remove_tile(self, tile: Tile):
         for i in self.tiles:
             if i.letter == tile.letter:
                 self.tiles.remove(i)
-                print(f"Tile {i.letter} eliminado.")
                 break
+
+    
 
 
 
